a7f6d85c079431cb30886e74026e4c785091a3ee
CheolungKang <cheolung86@gmail.com>
2021-05-10 18:00:28 +0900

    Modify the python file name in the cmd file
    Fixed an error where the cmd file name and python file name did not match.

--- Run_AzurePnP_application_test_ST102.cmd
+++ Run_AzurePnP_application_test_ST102.cmd
@@ -3,4 +3,4 @@ set IOTHUB_DEVICE_DPS_ID_SCOPE=
 set IOTHUB_DEVICE_DPS_DEVICE_ID=
 set IOTHUB_DEVICE_DPS_DEVICE_KEY=
 set IOTHUB_DEVICE_DPS_ENDPOINT=
-python simple_device_RT101.py
+python simple_device_ST102.py
